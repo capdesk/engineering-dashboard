70cd9ca28ddffb4dc40a8a8667771c4823aa6b47
Marc-Antoine Ferland <me@maferland.com>
2022-02-09 22:11:48 -0500

    feat(): cleanup style props

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,16 +11,12 @@ import { Radar } from '~/components/Radar'
 import { Button } from '~/components/Button'
 
 const Navbar: React.FC = () => (
-  <div className="flex flex-row padding-nav items-center justify-between">
-    <div className="flex flex-row items-center gap-8 ">
+  <div className="flex flex-row p-16 items-center justify-between">
+    <div className="flex flex-row items-center gap-8 w-1/2">
       <img width="250" height="250" src={engineering} />
       <div className="flex flex-col">
         <img width="131" src={capdesk} />
-        <h1 className="font-mono text-2xl">
-          Engineers
-          <br />
-          Dashboard
-        </h1>
+        <h1 className="font-mono text-2xl">Engineers Dashboard</h1>
       </div>
     </div>
     <Paragraph />
@@ -28,15 +24,15 @@ const Navbar: React.FC = () => (
 )
 
 const Paragraph: React.FC = () => (
-  <div className="font-mono flex flex-col gap-6" style={{ width: '460px' }}>
+  <div className="font-mono flex flex-col gap-5 w-1/2">
     <div className="flex flex-row gap-5">
       <Button inverted>NAV 1</Button>
       <Button inverted />
       <Button>NAV 2</Button>
       <Button inverted>NAV 3</Button>
     </div>
-    <h2 className="font-sans">Here's a title style in trim</h2>
-    <p className="font-sans">
+    <h2 className="font-sans text-3xl">Here's a title style in trim</h2>
+    <p className="font-sans text-xl">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className="text-highlight-on">sed do eiusmod</span>
       &nbsp;tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
     </p>
@@ -44,10 +40,7 @@ const Paragraph: React.FC = () => (
 )
 const RadarSection: React.FC = () => {
   return (
-    <div
-      className="flex flex-row justify-center items-center gap-8 padding-large bg-white"
-      style={{ height: '600px', padding: '63px 110px' }}
-    >
+    <div className="flex flex-row justify-center items-center gap-8 bg-white py-16 px-28" style={{ height: '600px' }}>
       <Radar />
     </div>
   )
@@ -55,10 +48,7 @@ const RadarSection: React.FC = () => {
 
 const CalendarSection: React.FC = () => {
   return (
-    <div
-      className="flex flex-row justify-center items-center gap-8 padding-large bg-white"
-      style={{ height: '600px', padding: '63px 110px' }}
-    >
+    <div className="flex flex-row justify-center items-center gap-8 bg-white py-16 px-28" style={{ height: '600px' }}>
       <Calendar />
     </div>
   )
@@ -73,7 +63,7 @@ const Stats: React.FC = () => {
   ]
   const weights = ['black', 'bold', 'normal', 'thin']
   return (
-    <div className="flex flex-row justify-between items-center gap-8 padding-large " style={{ padding: '63px 110px' }}>
+    <div className="flex flex-row justify-between items-center gap-8 py-16 px-28">
       {stats.map(([percentage, date], index) => (
         <div key={date} className="flex flex-col">
           <p className={`text-7xl text-highlight-on font-${weights[index]}`}>{percentage}</p>
@@ -92,10 +82,7 @@ const Tools: React.FC = () => {
     launchdarkly,
   }
   return (
-    <div
-      className="flex flex-row justify-between items-center gap-8 padding-large bg-white"
-      style={{ padding: '63px 110px' }}
-    >
+    <div className="flex flex-row justify-between items-center gap-8 bg-white py-16 px-28">
       {Object.entries(tools).map(([key, value]) => (
         <img key={key} src={value} />
       ))}
@@ -104,7 +91,7 @@ const Tools: React.FC = () => {
 }
 
 const Footer: React.FC = () => (
-  <div className="flex flex-row items-center gap-8 padding-nav" style={{ height: '172px' }}>
+  <div className="flex flex-row items-center gap-8 p-16 h-44">
     <img width="84" src={engineeringSmall} />
   </div>
 )
