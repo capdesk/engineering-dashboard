b3bab6194d8612b76946b16ecc617e1930c9c2a1
Marc-Antoine Ferland <me@maferland.com>
2022-04-13 19:46:56 -0400

    feat(): responsive navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,12 +3,15 @@ import capdesk from '~/assets/images/capdesk.svg'
 import { Button } from '~/components/Button'
 
 const Navbar: React.FC = () => (
-  <div className="flex flex-row py-8 px-16 items-center justify-between bg-dark-300">
-    <Link to="/">
-      <img height="37" src={capdesk} />
-    </Link>
-    <p className="font-mono text-2xl">Engineering Dashboard</p>
-    <div className="flex flex-row gap-4 w1/3">
+  <div className="w-full grid grid-cols-2 lg:grid-cols-3 items-center py-8 px-16 bg-dark-300">
+    <div className="flex flex-col flex-grow-0">
+      <Link to="/">
+        <img height="37" src={capdesk} />
+      </Link>
+      <p className="lg:hidden font-mono text-2xl">Engineering Dashboard</p>
+    </div>
+    <p className="hidden lg:inline font-mono text-2xl text-left md:text-center">Engineering Dashboard</p>
+    <div className="flex flex-col md:flex-row gap-4 justify-end">
       <Link to="/resources">
         <Button>RESOURCES</Button>
       </Link>
