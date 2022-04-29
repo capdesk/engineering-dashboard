135c4124b7871e37520fdcc887fec9d52ad33982
Marc-Antoine Ferland <me@maferland.com>
2022-04-29 13:10:43 -0400

    fix(): add missing configs

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,7 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "api/_build",
-  ignoredRouteFiles: [".*"]
-};
+  serverBuildTarget: 'netlify',
+  server: './server.js',
+  ignoredRouteFiles: ['**/.*'],
+}
