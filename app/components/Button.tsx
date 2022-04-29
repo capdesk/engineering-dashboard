938a0c1e77e5a85f7a04a9dac404f60b8c3c13ae
Marc-Antoine Ferland <me@maferland.com>
2022-04-29 11:15:07 -0400

    feat(): ad active state

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,7 @@
-const Button: React.FC<{ inverted?: boolean }> = ({ children, inverted }) => {
-  return <div className={`button flex flex-row items-center justify-center w-32`}>{children}</div>
+const Button: React.FC<{ active?: boolean }> = ({ children, active }) => {
+  return (
+    <div className={`button flex flex-row items-center justify-center w-32 ${active ? 'active' : ''}`}>{children}</div>
+  )
 }
 
 export { Button }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,23 @@
-import { Link } from 'remix'
+import { useLocation, useMatch } from 'react-router'
+import { Link, LinkProps, useResolvedPath } from 'remix'
 import capdesk from '~/assets/images/capdesk.svg'
 import { Button } from '~/components/Button'
+
+const useActive = (to: To) => {
+  const resolved = useResolvedPath(to)
+  const match = useMatch({ path: resolved.pathname, end: true })
+  return Boolean(match)
+}
+
+const NavLink: React.FC<LinkProps> = ({ to, children, ...props }) => {
+  const isActive = useActive(to)
+
+  return (
+    <Link to={to} {...props}>
+      <Button active={isActive}>{children}</Button>
+    </Link>
+  )
+}
 
 const Navbar: React.FC = () => (
   <div className="w-full grid grid-cols-2 justify-between items-center py-8 px-7 lg:px-16 bg-dark-300">
@@ -9,12 +26,8 @@ const Navbar: React.FC = () => (
     </Link>
 
     <div className="flex flex-col md:flex-row gap-4 justify-end items-end">
-      <Link to="/resources">
-        <Button>RESOURCES</Button>
-      </Link>
-      <Link to="/team">
-        <Button inverted>TEAM</Button>
-      </Link>
+      <NavLink to="/resources">RESOURCES</NavLink>
+      <NavLink to="/team">TEAM</NavLink>
     </div>
   </div>
 )
